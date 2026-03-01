272aac51ef63ffa1107e1776d25130277831e363
Alastair Bilby <bilby@knoobgroup.com>
2026-03-01 15:43:46 +1100

    Add options condition.

--- autofixture/src/fixture/mod.rs
+++ autofixture/src/fixture/mod.rs
@@ -9,23 +9,32 @@ use crate::fixture::{
     builder::FixtureBuilder,
 };
 
+/// The main `Fixture` structure that maintains any state and the RNG engine.
 pub struct Fixture {
     rng: ThreadRng,
 }
 
 impl Fixture {
+    /// Creates a new `Fixture` with a default RNG engine.
     pub fn new() -> Self {
         Self { rng: ThreadRng::default() }
     }
 
+    /// Creates a new `FixtureBuilder` for type `B`, where `B` is a
+    /// type that implements `FixtureBuilder`. This includes all Rust primitive
+    /// types, and can be derived on a struct or enum with `#[derive(AutoFixture)]`.
     pub fn build<'b, B: FixtureBuilder<'b>>(&'b mut self) -> B {
         B::new(self)
     }
 
+    /// Creates a new randomly populated implementation of the given type `F`
+    /// where `F` implements `AutoFixture`. This includes all Rust primitive
+    /// types, and can be derived on a struct or enum with `#[derive(AutoFixture)]`
     pub fn create<F: AutoFixture>(&mut self) -> F {
         F::create(self)
     }
 
+    /// Return the `Fixture` implementations `ThreadRng` engine.
     pub fn rng(&mut self) -> &mut ThreadRng {
         &mut self.rng
     }
