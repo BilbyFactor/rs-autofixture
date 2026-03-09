c6a57183cfca9cf37b4b907906107acc2abc111c
Alastair Bilby <bilby@knoobgroup.com>
2026-03-10 00:04:43 +1100

    Fix nightly floats.

--- autofixture/src/fixture/primitives/floating_point.rs
+++ autofixture/src/fixture/primitives/floating_point.rs
@@ -10,6 +10,6 @@ impl_autofixture_random!(f32 => f32Builder, f64 => f64Builder);
 
 #[cfg(feature = "nightly-float")]
 pub mod nightly_float {
-    impl_autofixture_random!(f16, f128);
     create_numeric_builder!(f16, f128);
+    impl_autofixture_random!(f16 => f16Builder, f128 => f128Builder);
 }
